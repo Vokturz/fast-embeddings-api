3a8ff56541ad219e77ec37df665c4b5f62ff84a4
Vokturz <victornavarrokp47@gmail.com>
2023-09-25 19:16:25 -0300

    set 0.0.0.0 as default HOST

--- src/server/__main__.py
+++ src/server/__main__.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     app = create_app()
 
     uvicorn.run(
-        app, host=os.getenv("HOST", "localhost"), port=int(os.getenv("PORT", 8000))
+        app, host=os.getenv("HOST", "0.0.0.0"), port=int(os.getenv("PORT", 8000))
     )
 
 
